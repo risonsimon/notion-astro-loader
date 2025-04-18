9ad4219f312529df4624cf5a21d34219e93e6aec
Nathan Cho <ntcho13@gmail.com>
2025-04-18 13:00:00 -0400

    docs: add documentation for image pipeline

--- src/image.ts
+++ src/image.ts
@@ -11,7 +11,32 @@ export interface SaveOptions {
 
 export const VIRTUAL_CONTENT_ROOT = 'src/content/notion';
 
-// example: https://prod-files-secure.s3.us-west-2.amazonaws.com/ed3b245b-dd96-4e0d-a399-9a99a0cf37c0/d16195b7-f998-4b7c-8d2e-38b9c47be295/image.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45FSPPWI6X/20250123/us-west-2/s3/aws4_request&X-Amz-Date=20250123T145339Z&X-Amz-Expires=3600&X-Amz-Signature=c67284d3172499aa2f41faa55710f81ec49f53687c500ad1a794b2c0410d477f&X-Amz-SignedHeaders=host&x-id=GetObject
+/**
+ * Downloads and saves an image from an AWS URL to a local directory.
+ *
+ * @param url The AWS URL of the image to download.
+ * @param dir The directory path where the image will be saved.
+ * @param options Optional configuration for saving the image.
+ * @param options.ignoreCache Whether to ignore cached images and download again. Defaults to false.
+ * @param options.log Optional logging function to record the operation.
+ * @param options.tag Optional tagging function to mark the operation as 'download' or 'cached'.
+ *
+ * @returns The relative path of the saved image from the project's virtual content root.
+ *
+ * @throws {Error} If the provided `url` is invalid.
+ *
+ * @example
+ * For the following AWS URL:
+ * https://prod-files-secure.s3.us-west-2.amazonaws.com/ed3b245b-dd96-4e0d-a399-9a99a0cf37c0/d16195b7-f998-4b7c-8d2e-38b9c47be295/image.png?...
+ *
+ * The function parses the URL into:
+ * - Parent ID: ed3b245b-dd96-4e0d-a399-9a99a0cf37c0
+ * - Object ID: d16195b7-f998-4b7c-8d2e-38b9c47be295
+ * - File Name: image.png
+ *
+ * And saves it to:
+ * ./src/{dir}/ed3b245b-dd96-4e0d-a399-9a99a0cf37c0/d16195b7-f998-4b7c-8d2e-38b9c47be295.png
+ */
 export async function saveImageFromAWS(url: string, dir: string, options: SaveOptions = {}) {
   const { ignoreCache, log, tag } = options;
 
@@ -56,6 +81,9 @@ export async function saveImageFromAWS(url: string, dir: string, options: SaveOp
   return path.relative(relBasePath, filePath);
 }
 
+/**
+ * Transforms a raw image path into a relative path from the 'src' directory.
+ */
 export function transformImagePathForCover(rawPath: string): string {
   // get abs path from relative path to VIRTUAL_CONTENT_ROOT
   const absPath = path.resolve(process.cwd(), VIRTUAL_CONTENT_ROOT, rawPath);
